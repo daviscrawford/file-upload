1eee66851e2ce52b05d546281ef631c0c5fc91c1
Davis Crawford <davis1@vt.edu>
2019-08-06 13:38:01 -0400

    added upload button

--- src/App.js
+++ src/App.js
@@ -6,22 +6,33 @@ function App() {
   return (
     <div class="container">
       <div class="row">
-     <div class="col-md-3">
+        <div class="col-md-3">
 
-     </div>
-	   <div class="col-md-6">
-	      <form method="post" action="#" id="#">
-          
-          <div class="form-group files color">
-            <label>Upload Your File </label>
-            <input type="file" class="form-control" multiple=""/>
-          </div>
-        
-        </form>
+        </div>
+        <div class="col-md-6">
+          <form method="post" action="#" id="#">
+
+            <div class="form-group files color">
+              <label>Upload Your File </label>
+              <input type="file" class="form-control" multiple="" />
+            </div>
+
+          </form>
+        </div>
+        <div class="col-md-3">
+
+        </div>
       </div>
-      <div class="col-md-3">
-       
-     </div>
+      <div class="row">
+        <div class="col-md-3">
+
+        </div>
+        <div class="col-md-6">
+          <button class="upload-button">Upload</button>
+        </div>
+        <div class="col-md-3">
+
+        </div>
       </div>
     </div>
   );
