79081f1b777fb6e8cd7f44afa4493d017025446a
Davis Crawford <davis1@vt.edu>
2019-08-06 13:03:52 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="container">
+	   <div class="col-md-6">
+	      <form method="post" action="#" id="#">
+          
+          <div class="form-group files color">
+            <label>Upload Your File </label>
+            <input type="file" class="form-control" multiple=""/>
+          </div>
+        
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
